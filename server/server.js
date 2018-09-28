98ec1f249f2df5b54cd89217a09ac8f7dc658b9e
Imonjirie Newton <newtonimonjirie@gmail.com>
2018-09-28 10:38:40 +0100

    initial commit

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const publicPath = path.join(__dirname, '../public');
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+
+app.use(express.static(publicPath));
+
+// app.get('/', (req, res) => {
+//     res.sendFile(publicPath + '/index.html');
+// });
+
+app.listen(port, (req, res) => {
+    console.log(`Server is now listening on port ${port}`);
+});
