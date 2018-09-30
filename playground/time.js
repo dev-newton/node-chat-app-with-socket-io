f83c7647a26c6d749bc2a75d7f13d43ded4ad2b2
Imonjirie Newton <newtonimonjirie@gmail.com>
2018-09-30 11:38:47 +0100

    Add Mustache js for message templates

---
+++ playground/time.js
@@ -0,0 +1,8 @@
+var moment = require('moment');
+// var date = new Date();
+
+// console.log(date.getMonth());
+
+var createdAt = 1234;
+var date = moment(createdAt);
+console.log(date.format('h:mm a dddd MMMM Do, YYYY'))
